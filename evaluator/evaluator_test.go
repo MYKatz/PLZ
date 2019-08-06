4654139b3238b58cddf0e991ae103f5562eeeb01
Matt Katz <mykatz01@gmail.com>
2019-08-05 20:20:45 -0700

    Fully-working interpreter!

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -58,8 +58,9 @@ func testEval(inp string) object.Object {
 	l := lexer.NewLexer(inp)
 	p := parser.NewParser(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, evaluated object.Object, expected int64) bool {
@@ -164,6 +165,7 @@ func TestErrorHandling(t *testing.T) {
 		{"5 + True plz 5 plz", "type mismatch: INTEGER + BOOLEAN"},
 		{"-True plz", "unknown operator: -BOOLEAN"},
 		{"True + False plz", "unknown operator: BOOLEAN + BOOLEAN"},
+		{"foo plz", "identifier not found: foo"},
 	}
 
 	for _, tt := range tests {
@@ -178,5 +180,63 @@ func TestErrorHandling(t *testing.T) {
 		if err.Message != tt.expected {
 			t.Errorf("wrong error message. Expected %q, received %q", tt.expected, err.Message)
 		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a be 5 plz a plz", 5},
+		{"let a be 5 * 5 plz a plz", 25},
+		{"let a be 5 plz let b be a plz b plz", 5},
+		{"let a be 5 plz let b be a plz let c be a + b + 5 plz c plz", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func TestFunctionObject(t *testing.T) {
+	input := "function(x, y) please x + y plz thanks plz"
+
+	evaluated := testEval(input)
+	fn, ok := evaluated.(*object.Function)
+	if !ok {
+		t.Fatalf("Object is not a function. Received type %T", evaluated)
+	}
+
+	if len(fn.Parameters) != 2 {
+		t.Fatalf("Function has wrong number of parameters. Expected 2, received %d", len(fn.Parameters))
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("First parameter is not 'x', got %q", fn.Parameters[0].String())
+	}
+
+	if fn.Parameters[1].String() != "y" {
+		t.Fatalf("First parameter is not 'y', got %q", fn.Parameters[0].String())
+	}
+
+	if fn.Body.String() != "(x+y)" {
+		t.Fatalf("Incorrect function body. Expected %s, received %s", "(x+y)", fn.Body.String())
+	}
+}
+
+func TestFunctions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let id be function(x) please x plz thanks plz id(5) plz", 5},
+		{"let id be function(x) please return x plz thanks plz id(5) plz", 5},
+		{"let double be function(x) please return x*2 plz thanks plz double(double(2))", 8},
+		{"function(x) please return x+1 plz thanks(5) plz", 6},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
