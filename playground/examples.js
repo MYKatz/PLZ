f66ba09dee3a33daf84563321a0f0bc851d5a159
Matt Katz <mykatz01@gmail.com>
2019-09-08 23:24:24 -0700

    Playground frontend

---
+++ playground/examples.js
@@ -0,0 +1,13 @@
+examples = {
+  fibonacci: `let fibonacci be function(x) please
+    if(x==0) please
+        return 0 plz
+    thanks
+    if(x==1) please
+        return 1 plz
+    thanks
+    return fibonacci(x - 1) + fibonacci(x - 2) plz
+thanks
+
+print(fibonacci(10))`
+};
