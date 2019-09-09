35451145998d321eb8880389035abf5b17b98ef1
Matt Katz <mykatz01@gmail.com>
2019-09-09 01:04:17 -0700

    More examples and cleanup

--- playground/examples.js
+++ playground/examples.js
@@ -1,4 +1,6 @@
 examples = {
+  helloWorld: `let msg be "Hello, world!" plz
+print(msg) plz`,
   fibonacci: `let fibonacci be function(x) please
     if(x==0) please
         return 0 plz
@@ -9,5 +11,15 @@ examples = {
     return fibonacci(x - 1) + fibonacci(x - 2) plz
 thanks
 
-print(fibonacci(10))`
+print(fibonacci(10))`,
+  sum: `let nums be [1, 2, 3, 4, 5]
+
+let sum be function(arr) please
+    if(len(arr) == 1) please
+        return arr[0] plz
+    thanks
+        return arr[0] + sum(rest(arr)) plz 
+    thanks
+    
+print(sum(nums)) plz`
 };
